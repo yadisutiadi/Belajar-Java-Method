695044860e7e7c1295f95271d7658473105875e5
YADI SUTIADI <yadisutiya@gmail.com>
2023-06-20 17:06:06 +0700

    tambahan return

--- src/main/java/YADI/parameterReturn.java
+++ src/main/java/YADI/parameterReturn.java
@@ -1,12 +1,29 @@
 package YADI;
 
 public class parameterReturn {
-    static void greeting(String nama){
-        System.out.println("HASIL PARAMETER :" + nama);
+
+    //contoh parameter
+   // static void greeting(String nama, Integer umur){
+     //   System.out.println("HASIL PARAMETER :" + nama + umur + "tahun");
+    //}
+
+    //contoh parameter menggunakan return
+    //contoh 1 dari method 1
+    static String greeting(String nama, Integer umur){
+        System.out.println("HASIL PARAMETER :" + nama + umur + "tahun");
+        return nama + umur;
+    }
+    //contoh 2 dari method 2
+    static int penjumlahan ( int a, int b){
+        return a + b ;
     }
 
     public static void main(String[] args) {
-        greeting("yadi");
-        greeting("iday");
+       String data = greeting("yadi", 25);
+        System.out.println(data);
+       String date = greeting("iday", 27);
+        System.out.println(date);
+        int hasil = penjumlahan(4,6);
+        System.out.println(hasil);
     }
 }
